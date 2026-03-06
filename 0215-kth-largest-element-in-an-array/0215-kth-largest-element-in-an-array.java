8e45da087e5881649057977b71c7ab2b984e2272
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-03-06 10:27:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,18 +1,53 @@
-import java.util.*;
-
 class Solution {
+
     public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public int quickSelect(int[] arr, int start, int end, int k) {
 
-        for(int num : nums){
-            pq.offer(num);
+        int pivotIndex = partition(arr, start, end);
 
-            if(pq.size() > k){
-                pq.poll();
+        if (k < arr.length - pivotIndex) {
+            return quickSelect(arr, pivotIndex + 1, end, k);
+        } 
+        else if (k > arr.length - pivotIndex) {
+            return quickSelect(arr, start, pivotIndex - 1, k);
+        }
+
+        return arr[pivotIndex];
+    }
+
+    public int partition(int[] arr, int start, int end) {
+
+        int pivot = arr[end];
+        int i = start;
+        int j = end - 1;
+
+        while (i <= j) {
+
+            while (i <= j && arr[i] < pivot) {
+                i++;
+            }
+
+            while (i <= j && arr[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                swap(arr, i, j);
+                i++;
+                j--;
             }
         }
 
-        return pq.peek();
+        swap(arr, i, end);
+        return i;
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
