cd10c6d9f28d4bb23dbcd50a64232d882bebe5d6
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-03-11 08:05:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,32 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(mp.containsKey(nums[i]))
-            {
-                int idx = mp.get(nums[i]);
-                if((i-idx)<=k)
-                    return true;
-                else
-                    mp.put(nums[i],i);
-                }else
-                    mp.put(nums[i],i);
-                }
+        // HashMap<Integer,Integer> mp = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(mp.containsKey(nums[i]))
+        //     {
+        //         int idx = mp.get(nums[i]);
+        //         if((i-idx)<=k)
+        //             return true;
+        //         else
+        //             mp.put(nums[i],i);
+        //         }else
+        //             mp.put(nums[i],i);
+        //         }
             
+        // return false;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],i);
+            }else{
+                int diff = Math.abs(map.get(nums[i])-i);
+                if(diff <= k){
+                    return true;
+                }else{
+                    map.put(nums[i],i);
+                }
+            }
+        }
         return false;
     }
 }
