0092281450e8d6407c1300adca8b1f89754feb89
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-03-25 21:52:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,15 +1,38 @@
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int[] nge = new int[nums.length];
+//         Stack<Integer> st = new Stack<>();
+
+//         st.push(0);
+//         for(int i=1;i<nums.length;i++){
+//             while(st.size() > 0 && nums[i] > nums[st.peek()]){
+//                 int pos = st.peek();
+//                 nge[pos] = nums[i];
+//                 st.pop();
+//             }
+//             st.push(i);
+//         }
+//         while(st.size()>0){
+//             int pos = st.peek();
+//             nge[pos] = -1;
+//             st.pop();
+//         }
+//         return nge;
+        
+//     }
+// }
+
+
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] nge = new int[n];
-
-        // Manually initialize with -1
-        for(int i = 0; i < n; i++){
-            nge[i] = -1;
-        }
+        Arrays.fill(nge, -1); // default = -1
 
         Stack<Integer> st = new Stack<>();
 
+        // Traverse twice for circular behavior
         for(int i = 0; i < 2 * n; i++){
             int idx = i % n;
 
@@ -18,6 +41,7 @@ class Solution {
                 st.pop();
             }
 
+            // Only push in first pass
             if(i < n){
                 st.push(idx);
             }
