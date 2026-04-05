29ae42e5b716c75634c43fbd673f0e073c1ae2ff
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-04-05 11:04:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,42 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-    HashMap<String,List<String>> hm = new HashMap<>();
+        // if(strs == null || strs.length == 0)
+        //    return new ArrayList<>();
 
-    for(int i=0;i<strs.length;i++){
-        char[] ch = strs[i].toCharArray();
-        Arrays.sort(ch);
-        String sorted = new String(ch);
+        //    Map<String,List<String>> frq  = new HashMap<>();
+        //    for(String str : strs){
+        //     String frqSring = getFrequencyString(str);
 
-        if(!hm.containsKey(sorted)){
-            hm.put(sorted,new ArrayList<String>());
-        }
-        hm.get(sorted).add(strs[i]);
-    }
-    return new ArrayList<List<String>>(hm.values());
-        
+        //     if(frq.containsKey(freString)){
+        //         frq.get(frqString).add(str);
+        //     }
+        //     else{
+        //         List<String> strList = new ArrayList<>();
+        //         strList.add(str);
+        //         frq.put(frqString,strList);
+        //     }
+        //    }
+        //    return new ArrayList<>(frq.values());
+
+
+         Map<String,List> map = new HashMap<>();
+         for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[c-'a']++;
+            }
+            StringBuilder sb  = new StringBuilder();
+            for(int i : count){
+                sb.append("#");
+                sb.append(i);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<String>());
+            }
+            map.get(key).add(s);
+         }
+         return new ArrayList(map.values());
+
     }
 }
