bcbfa03c6ee422e78de0187cea4bdbe41a554335
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-04-19 13:52:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,15 +14,13 @@
  * }
  */
 class Solution {
-    private TreeNode first;
-    private TreeNode prev;
-    private TreeNode middle;
-    private TreeNode last;
+     TreeNode first;
+     TreeNode prev;
+     TreeNode middle;
+     TreeNode last;
    void inorder(TreeNode root){
     if(root == null) return;
-
       inorder(root.left);
-
       if(prev != null && (root.val < prev.val))
       {
         if(first == null)
@@ -36,8 +34,6 @@ class Solution {
       prev = root;
       inorder(root.right);
    }
-
-
     public void recoverTree(TreeNode root) {
         first = middle = last = null;
         prev = new TreeNode(Integer.MIN_VALUE);
