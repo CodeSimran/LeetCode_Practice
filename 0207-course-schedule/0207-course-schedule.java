6ea6b076fb05355ac5c80cf4bbad17b83cca8f26
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-04-27 14:13:45 +0530

    Time: 7 ms (59.56%), Space: 47.1 MB (64.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -3,25 +3,26 @@ class Solution {
         int[] inDegree = new int[numCourses];
         List<List<Integer>> graph = new ArrayList<>();
         
-        for (int i = 0; i < numCourses; i++) graph.add(new ArrayList<>());
-        
-        for (int[] p : prerequisites) {
+        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
+
+        for(int[] p: prerequisites){
             graph.get(p[1]).add(p[0]);
             inDegree[p[0]]++;
         }
-        
+
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++)
-            if (inDegree[i] == 0) q.add(i);
-        
-        int count = 0;
-        while (!q.isEmpty()) {
-            int course = q.poll();
-            count++;
-            for (int next : graph.get(course))
-                if (--inDegree[next] == 0) q.add(next);
-        }
-        
+        for(int i=0;i<numCourses;i++)
+            if(inDegree[i] == 0) q.add(i);
+
+            int count =0;
+            while(!q.isEmpty()){
+                int course = q.poll();
+                count++;
+                for(int next : graph.get(course))
+                    if(--inDegree[next]==0) q.add(next);
+            }
+
         return count == numCourses;
+        
     }
 }
