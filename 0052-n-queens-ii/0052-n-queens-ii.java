aff9e2e866245256508f2fb5363e0b6f0bf27100
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-05-05 22:39:42 +0530

    Time: 1 ms (95.89%), Space: 41.9 MB (72.97%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,49 +1,32 @@
 class Solution {
-    public boolean isSafe(int board[][],int row,int col,int n){
-        int r = row;
-        int c = col;
-        while(r>=0 && c>=0){
-            if(board[r][c] == 1){
-                return false;
-            }
-            r--;
-            c--;
-        }
-        r = row;
-        c = col;
-        while(r<n && c>=0){
-            if(board[r][c] == 1){
-                return false;
-            }
-            r++;
-            c--;
-        }
-        r = row;
-        c = col;
-        while(c>=0){
-            if(board[r][c] == 1){
-                return false;
-            }
-            c--;
-        }
-        return true;
-    }
-    public int helper(int board[][],int col,int n){
-        if(col>= board.length){
-            return 1;
-        }
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(isSafe(board,i,col,n)){
-                board[i][col] = 1;
-                count += helper(board,col+1,n);
-                board[i][col] = 0;
+    public int help(int i,int n,int[] col,int[] topLeft,int[] topRight){
+        if(i==n) return 1;
+        int count=0;
+        for(int j=0;j<n;j++){
+            if((col[j]==0) && (topLeft[i-j+n-1] ==0) && (topRight[i+j]==0)){
+                col[j]=1;
+                topLeft[i-j+n-1]=1;
+                topRight[i+j]=1;
+
+                count += help(i+1,n,col,topLeft,topRight);
+
+                col[j]=0;
+                topLeft[i-j+n-1]=0;
+                topRight[i+j]=0;
             }
         }
         return count;
     }
     public int totalNQueens(int n) {
-        int board[][] = new int[n][n];
-        return helper(board,0,n);
+        int ans = 0;
+
+        int[] col = new int[n];
+        Arrays.fill(col,0);
+        int[] topLeft = new int[2*n];
+        Arrays.fill(topLeft,0);
+        int[] topRight = new int[2*n];
+        Arrays.fill(topRight,0);
+        
+        return help(0,n,col,topLeft,topRight);
     }
 }
