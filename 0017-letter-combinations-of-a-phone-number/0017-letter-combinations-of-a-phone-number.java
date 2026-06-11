c6b1740a3f5994f26b3692739cc900052887819d
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-06-11 09:22:33 +0530

    Time: 1 ms (54.27%), Space: 43.7 MB (34.04%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
-        
         if (digits == null || digits.length() == 0) {
             return res;
         }
-        
         Map<Character, String> digitToLetters = new HashMap<>();
         digitToLetters.put('2', "abc");
         digitToLetters.put('3', "def");
@@ -15,18 +13,14 @@ class Solution {
         digitToLetters.put('7', "pqrs");
         digitToLetters.put('8', "tuv");
         digitToLetters.put('9', "wxyz");
-        
         backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
-        
         return res;        
     }
-
     private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
         if (idx == digits.length()) {
             res.add(comb.toString());
             return;
         }
-        
         String letters = digitToLetters.get(digits.charAt(idx));
         for (char letter : letters.toCharArray()) {
             comb.append(letter);
